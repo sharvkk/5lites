f9ab602281a017f1af8f588e8f0e5212ceee543c
YATINDRA-07 <yatindra.jain.yj@gmail.com>
2020-04-05 00:12:53 +0530

    user reg to database added

--- app/src/main/java/com/example/a5lites/Login.java
+++ app/src/main/java/com/example/a5lites/Login.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.document.datatype.Document;
 
 public class Login extends AppCompatActivity {
     Button mButton;
@@ -38,16 +41,53 @@ public class Login extends AppCompatActivity {
                 String num = mnum.getText().toString();
                 String macadd = mmac.getText().toString();
                 if(num.length() == 10 && !macadd.equals("")){
-                   prefs = getSharedPreferences(CREDENTIALS,Context.MODE_PRIVATE);
-                    prefs.edit().putBoolean(LOG_CHECK_KEY, true).apply();
-                    prefs.edit().putString(PHONE_NO,num).apply();
-                    prefs.edit().putString(MAC,macadd).apply();
-                    Toast.makeText(getApplicationContext(),"number saved",Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                    startActivity(intent);
-                    finish();
+                    try {
+                        prefs = getSharedPreferences(CREDENTIALS, Context.MODE_PRIVATE);
+                        prefs.edit().putBoolean(LOG_CHECK_KEY, true).apply();
+                        prefs.edit().putString(PHONE_NO, num).apply();
+                        prefs.edit().putString(MAC, macadd).apply();
+                        Document document = new Document();
+                        document.put("MACid", macadd);
+                        document.put("status", "negative");
+                        document.put("phone", num);
+                        Integer temp = new PutDataAsyncTask().execute(document).get();
+                        System.out.println("O/P: " + temp);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                        Toast.makeText(getApplicationContext(),"number saved",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent);
+                        finish();
                 }
             }
         });
+    }
+    private class GetAllItemsAsyncTask extends AsyncTask<String, Void, com.amazonaws.mobileconnectors.dynamodbv2.document.datatype.Document> {
+        @Override
+        protected com.amazonaws.mobileconnectors.dynamodbv2.document.datatype.Document doInBackground(String... params) {
+            DatabaseAccess databaseAccess = DatabaseAccess.getInstance(Login.this);
+            return databaseAccess.getItem(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(com.amazonaws.mobileconnectors.dynamodbv2.document.datatype.Document documents) {
+        }
+
+    }
+
+    private class PutDataAsyncTask extends AsyncTask<Document, Void, Integer> {
+        @Override
+        protected Integer doInBackground(Document... params) {
+            DatabaseAccess databaseAccess = DatabaseAccess.getInstance(Login.this);
+            databaseAccess.putItem(params[0]);
+            System.out.println("In background!!!");
+            return 1;
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+        }
+
     }
 }
--- app/src/main/java/com/example/a5lites/RegisterNode.java
+++ app/src/main/java/com/example/a5lites/RegisterNode.java
@@ -14,5 +14,4 @@ public class RegisterNode {
         return doc;
     }
 
-
 }
